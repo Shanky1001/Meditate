a9ed25cc4d646a91cf50bfa1a5274d9bfdd44446
Shashank Rai <shashank.rai@buopso.com>
2024-12-25 01:46:29 +0530

    added dummy data

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,13 @@
-const OnboardingImage = require("./onboarding.jpeg");
+const OnboardingImage = require("./images/onboarding.jpeg");
+const Meditation1 = require("./images/meditate1.jpg");
+const Meditation2 = require("./images/meditate2.jpg");
+const Meditation3 = require("./images/meditate3.jpg");
+const Meditation4 = require("./images/meditate4.jpg");
+const Meditation5 = require("./images/meditate5.jpg");
+const Meditation6 = require("./images/meditate6.jpg");
+const Rocks = require("./images/rocks.jpg");
+const Tea = require("./images/tea.jpg");
+const Sleep1 = require("./images/sleep.jpg");
+const Sleep2 = require("./images/sleep2.jpg");
 
-export {OnboardingImage};
+export {OnboardingImage, Meditation1, Meditation2, Meditation3, Meditation4, Meditation5, Meditation6, Rocks, Tea, Sleep1, Sleep2};
